ebe55a419191017ab760c59f65a9c3f10b92acf6
Jakub Dundalek <dundalek@gmail.com>
2025-10-08 16:18:15 +0200

    add scip tests

---
+++ test/src/io/github/dundalek/stratify/scip_test.clj
@@ -0,0 +1,19 @@
+(ns io.github.dundalek.stratify.scip-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [io.github.dundalek.stratify.scip :as scip]))
+
+(deftest scip-go-graph
+  (let [index (scip/read-scip-index "test/resources/scip/go.scip")]
+    (is (= {:adj {"main.go" #{"greet/greet.go"}}}
+           (scip/->graph index)))))
+
+(deftest scip-rust-graph
+  (let [index (scip/read-scip-index "test/resources/scip/rust.scip")]
+    (is (= {:adj {"src/main.rs" #{"src/greeting.rs"}}}
+           (scip/->graph index)))))
+
+(deftest scip-typescript-graph
+  (let [index (scip/read-scip-index "test/resources/scip/typescript.scip")]
+    (is (= {:adj {"src/main.ts" #{"src/greeting.ts" "src/main.ts"}}}
+           (scip/->graph index)))))
